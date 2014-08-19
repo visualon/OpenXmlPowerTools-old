18af9c3d1f2e8b2fa30752a4d308d33587273c01
Michael Kriese <michael.kriese@visualon.de>
2014-08-19 12:26:47 +0200

    2.7.4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.7.4-beta1")]
+[assembly: AssemblyInformationalVersion("2.7.4")]
 [assembly: AssemblyFileVersion("2.7.4")]
